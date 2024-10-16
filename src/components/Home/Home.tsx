bb4b3db140eb3a2b2e8e33ad7d53e2c6181ac271
ismailemayej <salikbd.com@gmai.com>
2024-10-16 14:11:09 +0600

    Pagination work is done

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -13,40 +13,69 @@ export type Book = {
 };
 
 const Home = () => {
-  // data fetching use UseContext
+  // data fetching use UseContext---------------------------------
   const books = useContext(Context);
   const [currentPage, setCurrentPage] = useState(1);
-  const handlePageChange = (newPage: number) => {
-    setCurrentPage(newPage);
+  const [postPerPage] = useState(6);
+  // pagination------------------------------------
+  const LIndex = currentPage * postPerPage;
+  const FIndex = LIndex - postPerPage;
+  const records = books.slice(FIndex, LIndex);
+  const npage = Math.ceil(books.length / postPerPage);
+  const numbers = [...Array(npage + 1).keys()].slice(1);
+  // const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+  const prePage = () => {
+    if (currentPage !== FIndex) {
+      setCurrentPage(currentPage - 1);
+    }
   };
+  const nextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
+  const changeCPage = (number: number) => {
+    setCurrentPage(number);
+  };
+
   return (
     <div>
       <div className="grid lg:grid-cols-3 gap-2 md:grid-cols-2 grid-cols-1 px-7 py-2 shadow-lg bg-white rounded-lg">
         {/* data mapping */}
         {books ? (
-          books?.map((book) => <BookList key={book.id} book={book} />)
+          records.map((book) => <BookList key={book.id} book={book} />)
         ) : (
           <Loading />
         )}
-
         {/* pagination */}
       </div>
-      <div className="flex justify-center mt-4">
-        <button
-          onClick={() => handlePageChange(currentPage - 1)}
-          disabled={currentPage === 1}
-          className="px-4 py-2 bg-blue-500 text-white font-bold rounded hover:bg-blue-700 transition duration-300"
-        >
-          Previous
-        </button>
-        <span className="mx-2 text-lg font-bold">{currentPage}</span>
-        <button
-          onClick={() => handlePageChange(currentPage + 1)}
-          className="px-4 py-2 bg-blue-500 text-white font-bold rounded hover:bg-blue-700 transition duration-300"
-        >
-          Next
-        </button>
-      </div>
+      <nav className="border p-1">
+        <ul className="flex justify-center items-center text-center border">
+          {/* prev page button */}
+          <li className="border-r p-1.5 text-center hover:bg-slate-300 hover:border hover: rounded-lg bg-blue-500 text-white px-6 hover:text-black">
+            <a href="#" onClick={prePage}>
+              Prev
+            </a>
+          </li>
+          {numbers.map((number) => (
+            <li
+              className={`border-r p-1.5 transition-all will-change-scroll items-center hover:bg-blue-500 hover:text-white px-4 rounded-lg ${
+                currentPage === number
+                  ? "bg-blue-500 text-white rounded-lg px-4"
+                  : ""
+              }`}
+              onClick={() => changeCPage(number)}
+              key={number}
+            >
+              {number}
+            </li>
+          ))}
+          {/* next page button */}
+          <li className="border-r p-1.5 text-center hover:bg-slate-300 hover:border hover: rounded-lg bg-blue-500 text-white px-6 hover:text-black">
+            <a href="#" onClick={nextPage}>
+              Next
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,35 +1,14 @@
-import { useEffect, useState } from "react";
-
-interface Book {
-  id: number;
-  title: string;
-}
+import { useContext, useState } from "react";
+import { Context } from "../../App";
 
 const Navbar = () => {
-  const [datas, setDatas] = useState<Book[]>([]);
+  const datas = useContext(Context);
   const [value, setValue] = useState<string>("");
-  useEffect(() => {
-    try {
-      fetchData({
-        target: { value: "" },
-      } as React.ChangeEvent<HTMLInputElement>);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  }, []);
   // data handle change On search box
-  const fetchData = async (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
-    try {
-      const response = await fetch(
-        `https://gutendex.com/books?search=${value}`
-      );
-      const data = await response.json();
-      setDatas(data.results as Book[]);
-    } catch (error) {
-      console.error("There was a problem with the fetch operation:", error);
-    }
   };
+
   return (
     <div>
       <nav className="flex justify-between items-center shadow-xl lg:mx-14 px-7 lg:py-3 py-1 rounded-md border">
@@ -37,26 +16,37 @@ const Navbar = () => {
         <div className="logo">Logo</div>
         <div className="flex gap-1 items-center mr-1">
           {/* Search box */}
-          <input
-            type="search"
-            placeholder="Search..."
-            onChange={fetchData}
-            value={value}
-            className="py-2 text-sm text-gray-700 w-full pr-3 pl-3 bg-slate-100 rounded-md"
-          />
-          <button className="bg-yellow-500 p-2 rounded-md text-white">
-            Search
-          </button>
+          <form>
+            <input
+              type="text"
+              placeholder="Search..."
+              value={value}
+              onChange={handleInputChange}
+              className="py-2 text-sm text-gray-700 w-full pr-3 pl-3 bg-slate-100 rounded-md"
+            />
+          </form>
         </div>
       </nav>
+      {/* filter and maping for data show in UI */}
       {value &&
         datas
-          .filter((item) => item?.title.startsWith(value))
+          .filter((item) =>
+            item?.title?.toLowerCase().includes(value.toLowerCase())
+          )
+          .slice(0, 7)
           .map((item) => (
             <div className="mx-16 my-3 border-b" key={item.id}>
-              {item.title}
+              <p>{item?.title}</p>
             </div>
           ))}
+      {value &&
+        datas.filter((item) =>
+          item?.title?.toLowerCase().includes(value.toLowerCase())
+        ).length === 0 && (
+          <div className="text-red-500 text-2xl text-center">
+            No Search Data
+          </div>
+        )}
     </div>
   );
 };
